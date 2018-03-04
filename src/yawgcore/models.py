d64c4f54e5e5b6dc4cc31cac21ecdb97020a7497
Jan Krajdl <spm@spamik.cz>
2018-03-04 17:44:18 +0100

    Model changes for gallery items
    - added models for albums and gallery items
    - mapping items with content type framework

--- src/yawgcore/models.py
+++ src/yawgcore/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -6,11 +8,33 @@ class GalleryStorage(models.Model):
     path = models.CharField(max_length=256)
 
 
+class ItemMapping(models.Model):
+    parent_ct = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='mapping_parent')
+    parent_id = models.PositiveIntegerField()
+    parent_object = GenericForeignKey('parent_ct', 'parent_id')
+    item_ct = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='mapping_item')
+    item_id = models.PositiveIntegerField()
+    item_object = GenericForeignKey('item_ct', 'item_id')
+
+
 class Gallery(models.Model):
     gallery_name = models.CharField(max_length=128)
     gallery_alias = models.CharField(max_length=64, unique=True)
+    items = GenericRelation(ItemMapping, content_type_field='parent_ct', object_id_field='parent_id')
 
 
 class GalleryDomainMap(models.Model):
     domain_host = models.CharField(max_length=128, unique=True)
     gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=128)
+    album_alias = models.CharField(max_length=64)
+    items = GenericRelation(ItemMapping, content_type_field='parent_ct', object_id_field='parent_id')
+
+
+class GalleryItem(models.Model):
+    item_name = models.CharField(max_length=128)
+    filename = models.CharField(max_length=64)
+
